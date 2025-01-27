b56849c087806881313da5d27e75e61d3a8dc595
danielnorberg1 <danno@student.chalmers.se>
2025-01-27 21:46:19 +0100

    Add a counter of the number of guesses

--- WP1/Exercise3/main.c
+++ WP1/Exercise3/main.c
@@ -1,11 +1,27 @@
 #include <stdlib.h>
+#include <stdio.h>
 
-#define UPPER_NUMBER 100
+#define MAX_NUMBER 100
 
 
 int main(){
 
-int randomNum = (rand() % UPPER_NUMBER) + 1;
+int randomNum = (rand() % MAX_NUMBER) + 1;
+char userInput;
+int timesGuessed = 0;
+
+printf("Guess a number between 0-100: \n");
+
+
+while(fgets(&userInput, MAX_NUMBER, stdin)){
+    timesGuessed++;
+
+  printf("%i", timesGuessed);
+
+
+}
+
+
 
 return 0;
 
