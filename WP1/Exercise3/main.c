af8dbbf952b6afda1639f44f8d66a9e7fe77e0a3
danielnorberg1 <danno@student.chalmers.se>
2025-01-27 22:51:53 +0100

    Promt user with message about guess

--- WP1/Exercise3/main.c
+++ WP1/Exercise3/main.c
@@ -1,25 +1,50 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <time.h>
 
-#define MAX_NUMBER 100
+#define MAX_LENGTH 100
+#define MAX_NUMBER 10
 
 
 int main(){
+srand(time(NULL));
 
-int randomNum = (rand() % MAX_NUMBER) + 1;
-char userInput;
+
+
+int randomNum = rand() % 101;
+char guess;
 int timesGuessed = 0;
+char input[MAX_LENGTH+1];
+
 
 printf("Guess a number between 0-100: \n");
 
+fgets(&input, MAX_LENGTH, stdin);
+guess = atoi(&input); 
 
-while(fgets(&userInput, MAX_NUMBER, stdin)){
+
+while(timesGuessed < MAX_NUMBER){
     timesGuessed++;
 
-  printf("%i", timesGuessed);
+    if ((guess > randomNum) && (guess < 101) && (guess > -1)) {
+        printf("Your guess is to high\n");
 
+    } else if ((guess < randomNum) && (guess < 101) && (guess > -1)){
+        printf("Your guess is to low\n");
+
+    } else {
+        printf ("You have guessed %i times and your guess is correct!\n", timesGuessed);
+        break;
+    }
+
+    printf("Guess a number between 0-100: \n");
+    fgets(&input, MAX_LENGTH, stdin);
+    guess = atoi(&input); 
 
 }
+
+
 
 
 
