0acbdf84948fb65499dcc6d162ee75886e9be1cd
danielnorberg1 <danno@student.chalmers.se>
2025-01-27 20:23:11 +0100

    Add comments

--- WP1/Exercise2/main.c
+++ WP1/Exercise2/main.c
@@ -8,33 +8,40 @@
 
 int main(int argc, char *argv[]){
 
+//Assigns the second argument to numberCharachterShift 
+// atoi converts it from characther to integer.
 int numberCharachterShift = atoi(argv[1]);
+
 char text[MAX_LENGTH];
 
 printf("Please write your sentence: \n");
 
+//Retrives argument from user with a maximum of MAX_LENGTH
+// Performs algorithm until EOF is reached.
 while (fgets(text, MAX_LENGTH, stdin)) {
 
 printf("Please write your sentence: \n");
 
 for (int i=0; text[i]; i++){
-    char charachter = text[i];
 
-    if (charachter >= 'A' && charachter <= 'Z'){
+    char charachter = text[i]; // Assign text charachter at given [i] possition to charachter.
+// Checks if charachter is Upper case
+    if (charachter >= 'A' && charachter <= 'Z'){ 
+// The charachter is shifted by the places in alphabet using the modolo operator and the given value.
         charachter = 'A' + ((charachter - 'A' + numberCharachterShift) % ALPHABET_LENGTH);
 
-     } else if (charachter >= 'a' && charachter <= 'z'){
+// Does the same as lines 29 to 31 but for lower case
+     } else if (charachter >= 'a' && charachter <= 'z'){ 
         charachter = 'a' + ((charachter - 'a' + numberCharachterShift) % ALPHABET_LENGTH);
     }
-    text[i] = charachter;
+    text[i] = charachter; // The text at [i] is assigned as charachter at [i].
 }
 
 
-printf("%s \n", text);
+printf("%s \n", text); // prints the text with encryption
 
 }
 
-return 0;
-
+return 0; // Returns 0 if program exits correctly
 
 }
