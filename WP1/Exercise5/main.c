8b32998e4f2de1c0ff6bd1d0709f6e18dc5aba28
danielnorberg1 <danno@student.chalmers.se>
2025-01-27 23:58:56 +0100

    Add skeleton provided by teacher

---
+++ WP1/Exercise5/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define MAX 100 // Defines the maximum number of the values in the table
+#define MAXNUMBER 20 // Defines the maximum value of random numbers
+
+
+// This function generates a set of random numbers
+// and fills the table *tab with these numbers
+void create_random(int *tab );
+
+
+
+// This function takes the *tab of random numbers
+// and creates a table with the frequency counts for these numbers
+void count_frequency(int *tab, int *freq );
+
+
+// This function takes the frequency count table
+// and draws a histogram of the values in that frequency table
+void draw_histogram(int *freq );
+// ------ Function definitions ----------
+// ------ Main --------------------------
+// The main entry point for the program
+//
+// If you choose to go for the optional part
+// Please modify it accordingly
+int main (void){
+
+int table[MAX], n ;
+
+
+int frequency[MAXNUMBER];
+}
