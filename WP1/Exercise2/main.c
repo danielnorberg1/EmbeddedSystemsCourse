09c8a7f80ccff59ccce4610b189dcced6a461b66
danielnorberg1 <danno@student.chalmers.se>
2025-01-27 19:51:24 +0100

    Check if charachter is Upper or Lower case

--- WP1/Exercise2/main.c
+++ WP1/Exercise2/main.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define ALPHABET_LENGTH 26
 #define MAX_LENGTH 100
 
 int main(int argc, char *argv[]){
@@ -15,6 +16,19 @@ char text[MAX_LENGTH];
 fgets(text, MAX_LENGTH, stdin);
 
 printf("%s \n", text);
+
+for (int i=0; text[i]; i++){
+    char charachter = text[i];
+    printf("%c \n", charachter);
+
+    if (charachter >= 'A' && charachter <= 'Z'){
+        
+    }
+
+    if(charachter >= 'a' && charachter <= 'z'){
+
+    }
+}
 
 
 
