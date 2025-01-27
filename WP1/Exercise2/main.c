0aa4510f894ef4e156a40ffadf415d314ca239c1
danielnorberg1 <danno@student.chalmers.se>
2025-01-27 19:49:19 +0100

    Ask user for a sentece and print

--- WP1/Exercise2/main.c
+++ WP1/Exercise2/main.c
@@ -2,7 +2,21 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define MAX_LENGTH 100
+
 int main(int argc, char *argv[]){
+
+
+
+printf("Please write your sentence: ");
+
+char text[MAX_LENGTH];
+
+fgets(text, MAX_LENGTH, stdin);
+
+printf("%s \n", text);
+
+
 
 
 return 0;
