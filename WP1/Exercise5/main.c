f2e787a30a9be1046cf74339c8c6df048a365f6a
danielnorberg1 <danno@student.chalmers.se>
2025-01-28 10:14:08 +0100

    Implement count_frequency function

--- WP1/Exercise5/main.c
+++ WP1/Exercise5/main.c
@@ -21,7 +21,19 @@ void create_random(int *tab ) {
 
 // This function takes the *tab of random numbers
 // and creates a table with the frequency counts for these numbers
-void count_frequency(int *tab, int *freq );
+void count_frequency(int *tab, int *freq ){
+    //This loop makes sure that there are no garbage values stored in freq[].
+    for (int i= 0; i < MAXNUMBER; i++ ){
+        freq[i] = 0;
+    }
+    //This loop get the value of each address in the tab array and increaments the given value 
+    // by 1 each itteration, and stores it in the freq array.
+    for (int i = 0; i < MAX; i++){
+        int number = tab[i];
+        freq[number]++;
+    }
+}
+
 
 // This function takes the frequency count table
 // and draws a histogram of the values in that frequency table
@@ -41,10 +53,12 @@ int table[MAX], n ;
 int frequency[MAXNUMBER];
 
 create_random(table);
+count_frequency(table, frequency);
 
 printf("%d", table);
 
 return 0;
 
 }
+
 
