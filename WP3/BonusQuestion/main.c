d5bdeabe8eb320d026fded795a7b4fc995802e79
danielnorberg1 <danno@student.chalmers.se>
2025-02-03 13:37:00 +0100

    Take input as size of matrices

---
+++ WP3/BonusQuestion/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h> // Includes necessary libraries
+
+
+
+int main(){
+
+printf("Input the size: ");
+
+int sizeOfMatrices = scanf("%i", sizeOfMatrices);
+
+
+int matrix1[sizeOfMatrices][sizeOfMatrices]; 
+int matrix2[sizeOfMatrices][sizeOfMatrices]; 
+int sumOfMatrices[sizeOfMatrices][sizeOfMatrices];
+
+
+
+
+return 0;
+}
