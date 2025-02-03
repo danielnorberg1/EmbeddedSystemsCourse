bbc90762fe7d9b4cc60635e15237beab4dfb991a
danielnorberg1 <danno@student.chalmers.se>
2025-02-03 15:27:12 +0100

    Refactor how depth and width input is handled

--- WP3/BonusQuestion/main.c
+++ WP3/BonusQuestion/main.c
@@ -1,42 +1,50 @@
 #include <stdio.h> // Includes necessary libraries
+#include <string.h>
 
-void clearBuffer(){
-    char lastChar;
-    while((lastChar = getChar()) != ("\n"));
-}
 
 int main(){
 
+int depthOfMatrices, widthOfMatrices;
+
 printf("Input the size: \n");
 
-int sizeOfMatrices = scanf("%i", sizeOfMatrices);
-clearBuffer();
+scanf("%i%i", &depthOfMatrices, &widthOfMatrices);
 
-int matrix1[sizeOfMatrices][sizeOfMatrices]; 
-int matrix2[sizeOfMatrices][sizeOfMatrices]; 
-int sumOfMatrices[sizeOfMatrices][sizeOfMatrices];
+
+
+
+
+int matrix1[depthOfMatrices][widthOfMatrices]; 
+int matrix2[depthOfMatrices][widthOfMatrices]; 
+int sumOfMatrices[depthOfMatrices][widthOfMatrices];
 
 int i;
 int j;
 
 printf("Input elements of matrix 1: \n");
-for (i = 0; i < sizeOfMatrices; i++) {
-    for (j = 0; j < sizeOfMatrices; j++){
-    scanf("%i", matrix1[i][j]);
-    clearBuffer();
+for (i = 0; i < depthOfMatrices; i++) {
+    for (j = 0; j < widthOfMatrices; j++){
+    scanf("%i", &matrix1[i][j]);
     }
 }
 
 
 printf("Input elements of matrix 2: \n");
-for (i = 0; i < sizeOfMatrices; i++) {
-    for (j = 0; j < sizeOfMatrices; j++){
-    scanf("%i", matrix2[i][j]);
-    clearBuffer();
+for (i = 0; i < depthOfMatrices; i++) {
+    for (j = 0; j < widthOfMatrices; j++){
+    scanf("%i", &matrix2[i][j]);
+    
     }
 }
 
+printf("The sum is: \n");
+for (i = 0; i < depthOfMatrices; i++){
+    for(j = 0; j < widthOfMatrices; j++){
+    sumOfMatrices[i][j] = (matrix1[i][j] + matrix2[i][j]); 
+    printf("%d\n", sumOfMatrices[i][j]);
+    }
 
+}
 
 
 
