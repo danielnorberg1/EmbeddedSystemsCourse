8c20b05d5698664e4cf5cecd953242a8ba4fd5a5
danielnorberg1 <danno@student.chalmers.se>
2025-02-12 23:48:09 +0100

    Swap two characthers

---
+++ WP4/BonusPoint/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+
+void swap(char *a, char *b){
+    char temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// Main function of the program
+int main(int argc, char * argv[]){
+
+
+    return 0;
+}
