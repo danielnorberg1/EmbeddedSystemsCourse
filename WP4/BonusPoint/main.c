fc9bff83eb03961c39140432221f6d9f0a11efac
danielnorberg1 <danno@student.chalmers.se>
2025-02-12 23:55:39 +0100

    Catch argument errors

--- WP4/BonusPoint/main.c
+++ WP4/BonusPoint/main.c
@@ -2,6 +2,8 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define MAX_CHARACTERS 10
+
 
 
 void swap(char *a, char *b){
@@ -10,8 +12,24 @@ void swap(char *a, char *b){
     *b = temp;
 }
 
+
+
 // Main function of the program
 int main(int argc, char * argv[]){
+
+    if (argc != 2){
+        printf("Must enter a argument");
+        return 1;
+    }
+
+    if (strlen(argv[1]) > MAX_CHARACTERS){
+        printf("The argument cannot exceed 10 charachters.");
+        return 1;
+    }
+
+
+    
+
 
 
     return 0;
